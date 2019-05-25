19ca58e74496f04a676c4709c3ce20672aba8a09
Ricardolv <richardluizv@gmail.com>
2019-05-25 01:29:51 -0300

    Authorization Filter

---
+++ gateway/src/main/java/com/richard/gateway/security/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.richard.gateway.security.config;
+
+import com.richard.core.property.JwtConfiguration;
+import com.richard.gateway.security.filter.GatewayJwtTokenAuthorizationFilter;
+import com.richard.token.security.config.SecurityTokenConfig;
+import com.richard.token.security.token.converter.TokenConverter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends SecurityTokenConfig {
+
+    private final TokenConverter tokenConverter;
+
+    public SecurityConfig(JwtConfiguration jwtConfiguration, TokenConverter tokenConverter) {
+        super(jwtConfiguration);
+        this.tokenConverter = tokenConverter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAfter(new GatewayJwtTokenAuthorizationFilter(jwtConfiguration, tokenConverter), UsernamePasswordAuthenticationFilter.class);
+        super.configure(http);
+    }
+}
