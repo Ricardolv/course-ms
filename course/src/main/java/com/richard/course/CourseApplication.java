5f702d28d5d8e866c2ad978fb815863ff50525f3
Ricardolv <richardluizv@gmail.com>
2019-05-25 02:18:55 -0300

    Authorization Filter pt 02

--- course/src/main/java/com/richard/course/CourseApplication.java
+++ course/src/main/java/com/richard/course/CourseApplication.java
@@ -1,13 +1,18 @@
 package com.richard.course;
 
+import com.richard.core.property.JwtConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EntityScan({"com.richard.core.model"})
 @EnableJpaRepositories({"com.richard.core.repository"})
+@EnableConfigurationProperties(value = JwtConfiguration.class)
+@ComponentScan({"com.richard"})
 public class CourseApplication {
 
     public static void main(String[] args) {
